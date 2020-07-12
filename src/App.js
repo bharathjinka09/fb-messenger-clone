818a2066f12da2f74ddb45b6f78276c3b7601b65
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-12 23:17:43 +0530

    Added messages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import Message from './Message'
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+
+  // useState = variable in REACT
+  // useEffect = run code on a specific condition (when data changes in db)
+
+  const sendMessage = (event) => {
+      // logic to send message
+      event.preventDefault()
+      setMessages([...messages, input]);
+      setInput('');
+  }
+
+  useEffect(() =>{
+    // run code
+    setUsername(prompt('Please enter your name'));
+    // If its blank inside [], this code runs ONCE when the app component loads
+    // If we have a varaible like input inside [], it runs when input changes everytime 
+  },[]) // condition dependencies
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FB Messenger Clone</h1>
+      <form>
+      <FormControl>
+        <InputLabel>Enter a message...</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)} />
+        <Button disabled={!input} size="small" type="submit" variant="contained" color="primary" onClick={sendMessage}>Send Message</Button>
+      </FormControl>
+
+      </form>
+
+      {
+        messages.map(message => (
+            <Message text={message} />
+          ))
+      }
+
     </div>
   );
 }
